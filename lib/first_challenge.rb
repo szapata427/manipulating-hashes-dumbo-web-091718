5c93d51db6e001a5c56e0b250a9d97243c6658ea
Steven Zapata <szapata427@gmail.com>
2018-09-10 03:27:14 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,7 @@ def first_challenge
     }
   }
 
-  #your code here
-
+contacts["Jon Snow"][:favorite_icecream_flavors] = "mint chip"
 
   #remember to return your newly altered contacts hash!
   contacts
