a76c636ab4ff812fd44496faf0c73ec55e9b5bc4
Steven Zapata <szapata427@gmail.com>
2018-09-10 03:51:10 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ contacts.each do |person, data|
     
     if attribute == :favorite_icecream_flavors
 
-        flavor.delete("strawberry")
+        value.delete("strawberry")
       end
     
 end
